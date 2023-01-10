6fdaa16863f5423c930183661acaea916bd48042
Bechir Majris <bmajri@gmail.com>
2023-01-10 18:45:45 +0100

    first commit

---
+++ controllers/player.py
@@ -0,0 +1,12 @@
+class Player:
+    def __init__(self, id, first_name, last_name, date_of_birth):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.date_of_birth = date_of_birth
+
+    def save_player(self):
+        pass
+
+    def delet_player(self):
+        pass
