f085b2e74c89e00993454ef2e8a2ae23f56b8e65
Bechir Majris <bmajri@gmail.com>
2023-01-19 18:34:48 +0100

    env a jours

---
+++ models/tournament_model.py
@@ -0,0 +1,18 @@
+class Tournament:
+    def __init__(self, tournament_name, place, start_date, end_date, curent_laps, laps_liste, list_players, description, total_laps=4):
+        self.tournament_name = tournament_name
+        self.place = place
+        self.start_date = start_date
+        self.end_date = end_date
+        self.curent_laps = curent_laps
+        self.laps_liste = laps_liste
+        self.liste_players = list_players
+        self.description = description
+        self.total_laps = total_laps
+
+    def creat_tournament(self):
+        tournament_liste = []
+        pass
+
+    def to_python(self):
+        pass
