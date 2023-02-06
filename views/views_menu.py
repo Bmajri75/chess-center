c71f232541a702394432c849c37271055f82c160
Bechir Majris <bmajri@gmail.com>
2023-02-06 20:08:11 +0100

    refonte de l'app, controller creat player mis en place views en cours

---
+++ views/views_menu.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Menu:
+    def ask_user(self):
+        """ask the user and return his response in menu_response"""
+        menu_response = int
+        value_menu = [0, 1, 2, 3, 4]
+        while menu_response not in value_menu:
+            print("""
+                    ||Bienvenue dans le Menu Generale
+                    ||selectionner une option, Que souhaitez vous faire""")
+
+            menu_response = int(input("""
+                      1 . Crée un joueur
+                      2 . Crée un tournoi
+                      3 . Voir un tournoi
+                      4 . Voir les rapports
+                      0 . Quit
+                      """))
+        return menu_response
+
+    def creat_player(self):
+        joueur = []
+        print("cree un joueur ")
+        id = input("Entre ton ID National>>>")
+        first_name = input("Entre ton Nom >>>")
+        last_name = input("Entre ton Prénom >>>")
+        date_of_birth = input("Entre ta date de naissance >>>")
+        joueur.append(id)
+        joueur.append(first_name, )
+        joueur.append(last_name, )
+        joueur.append(date_of_birth)
+        return print(joueur)
+
+    def match_response(self):
+        """match response from ask user and return the appropriate method """
+        match self.ask_user():
+            case 1:
+                print("reponse 1 ")
+                self.creat_player()
+            case 2:
+                print("reponse 2 ")
+            case 3:
+                print('reponse 3')
+            case 4:
+                print('reponse 4')
+            case 0:
+                print("reponse Q")
+
+
+menu = Menu()
+menu.match_response()
